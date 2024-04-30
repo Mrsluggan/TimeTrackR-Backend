3b55fcae041cc06647670dffe803e96b1864951d
mrsluggan <ericflyger@gmail.com>
2024-04-30 08:37:24 +0200

    first comit

---
+++ src/main/java/com/TimeTrackR/TimeTrackR/TimeTrackRApplication.java
@@ -0,0 +1,13 @@
+package com.TimeTrackR.TimeTrackR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TimeTrackR/TimeTrackR/TimeTrackRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeTrackR.TimeTrackR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
