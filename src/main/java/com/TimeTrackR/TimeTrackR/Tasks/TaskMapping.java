912af0be03a8722c539945dc5b13f3f77c1504fe
mrsluggan <ericflyger@gmail.com>
2024-04-30 09:01:26 +0200

    Taskmapping.java

---
+++ src/main/java/com/TimeTrackR/TimeTrackR/Tasks/TaskMapping.java
@@ -0,0 +1,10 @@
+package com.TimeTrackR.TimeTrackR.Tasks;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskMapping {
+
+    private TaskService taskService;
+
+}
