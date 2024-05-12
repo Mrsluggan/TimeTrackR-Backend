61fb70ea6609040cc55a6525cd652247667afee0
mrsluggan <ericflyger@gmail.com>
2024-05-12 22:58:45 +0200

    Update WebConfig.java

--- src/main/java/com/TimeTrackR/TimeTrackR/Configuration/WebConfig.java
+++ src/main/java/com/TimeTrackR/TimeTrackR/Configuration/WebConfig.java
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173") // Ange din frontend-URL här
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowCredentials(true);
     }
 }
