cc5b4a7391156e81bcb3760730753e4c6cc0991f
mrsluggan <ericflyger@gmail.com>
2024-05-12 22:06:18 +0200

    update

--- src/main/java/com/TimeTrackR/TimeTrackR/Configuration/WebConfig.java
+++ src/main/java/com/TimeTrackR/TimeTrackR/Configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Ange din frontend-URL här
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
