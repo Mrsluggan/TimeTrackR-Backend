1340917c1254dcf27b8a2d4df1ee4acbfd409f33
mrsluggan <ericflyger@gmail.com>
2024-05-16 10:29:44 +0200

    Update UserController.java

--- src/main/java/com/TimeTrackR/TimeTrackR/Model/Users/Controller/UserController.java
+++ src/main/java/com/TimeTrackR/TimeTrackR/Model/Users/Controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.TimeTrackR.TimeTrackR.Model.Users.Model.Users;
 import com.TimeTrackR.TimeTrackR.Model.Users.Service.UserService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
